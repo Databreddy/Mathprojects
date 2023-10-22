d26b38f8bf9f2ffce0b7c5214abec97b3022055f
reddyb <databreddy@gmail.com>
2023-10-22 13:21:34 +0200

    Version: 1. For ChatGPT.

---
+++ Armstrong_numbers.py
@@ -0,0 +1,13 @@
+def is_armstrong(number):
+    num_str = str(number)
+    num_digits = len(num_str)
+    armstrong_sum = sum(int(digit) ** num_digits for digit in num_str)
+    return armstrong_sum == number
+
+# Example usage:
+input_number = int(input("Enter a number: "))
+
+if is_armstrong(input_number):
+    print(f"{input_number} is an Armstrong number!")
+else:
+    print(f"{input_number} is not an Armstrong number.")
