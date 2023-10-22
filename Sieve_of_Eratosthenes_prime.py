8bec04aa1816c0a16c802e1eeef94a0cb49605a5
reddyb <databreddy@gmail.com>
2023-10-22 13:21:34 +0200

    Version: 1. For ChatGPT.

---
+++ Sieve_of_Eratosthenes_prime.py
@@ -0,0 +1,17 @@
+def sieve_of_eratosthenes(limit):
+    primes = [True] * (limit + 1)
+    primes[0] = primes[1] = False
+
+    for num in range(2, int(limit ** 0.5) + 1):
+        if primes[num]:
+            for multiple in range(num * num, limit + 1, num):
+                primes[multiple] = False
+
+    prime_numbers = [num for num, is_prime in enumerate(primes) if is_prime]
+    return prime_numbers
+
+
+# Example usage:
+limit = int(input("Enter the upper limit to find prime numbers: "))
+prime_numbers = sieve_of_eratosthenes(limit)
+print(f"Prime numbers up to {limit}: {prime_numbers}")
